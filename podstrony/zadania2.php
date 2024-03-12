ab5b0e6f95ea743e62910e18d02d553cd83c6fb8
GitHolo <ooye2332@gmail.com>
2024-03-12 10:44:46 +0100

    zad 1

---
+++ podstrony/zadania2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadania 2</title>
+</head>
+<body>
+    <h1>Zadania 12.03.2024</h1>
+    <!--zad 1-->
+    <h2>Zad 1</h2>
+    <?php
+    for ($i = 1; $i <= 10; $i++){
+        echo $i."<br>";
+    }?>
+</body>
+<style>
+    h2 {
+        margin-bottom: 0px;
+    } 
+</style>
+</html>
