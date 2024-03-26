d2da3d7bada253483adc55e2ecb2cddb170606a6
GitHolo <ooye2332@gmail.com>
2024-03-26 11:28:53 +0100

    db

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "zadanko";
+try {
+    $conn = mysqli_connect($host, $user, $pass , $database);
+
+    if(!$conn) {
+        echo "nie polaczono";
+    }
+
+    else {
+        echo "polaczono";
+    }
+}
+catch (mysqli_sql_exception){
+    echo "Xd";
+}
+?>
